829e676c501646c6772303832138cadfaae99108
Carlos Enciso <caenciso@unal.edu.co>
2021-10-15 08:59:32 -0500

    Mi primer programa e introduccion

---
+++ 2021-10-15-Intro/holamundo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+  std::cout <<"Hola mundo";
+  int a;
+  float x;
+  double y=0.0;
+  std::cout <<"a=" << a << "/n";
+  std::cout <<"x=" << x << "/n";
+  std::cout <<"y=" << y << "/n";
+  return 0;
+}
+// Edicion del codigo
+// Compilacion del codigo
+// Ejecucion del codigo
